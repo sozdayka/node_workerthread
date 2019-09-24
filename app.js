9cb8e51fe54b6bc59fa5b522e41dbb2042c63cf4
sozdayka <sozdayka@gmail.com>
2019-09-24 12:55:03 +0300

    sync

--- app.js
+++ app.js
@@ -5,42 +5,9 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello World 1</h1>');
 });
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
-
-  const { Worker, isMainThread,  workerData } = require('worker_threads');
-
-  if(isMainThread) {
-          console.log("this is the main thread")
-     
-          
-          let workerone = new Worker('./workerone.js',{ workerData: __dirname+'/original.jpg' });
-      
-          let workertwo = new Worker('./workertwo.js',{ workerData: __dirname+'/original.jpg' });
-  
-          workertwo.on('message',(data) => {
-              console.log("message",data);
-          })
-  
-          workertwo.on('error',(err) => {
-              console.error(err);
-          })
-          workerone.on('message',(data) => {
-              console.log("message",data)
-          })
-          
-          workerone.on('error',(err) => {
-              console.log(err);
-          })
-  
-          workerone.on('exit',(code) => {
-              if(code != 0) 
-                  console.error(`Worker stopped with exit code ${code}`)
-          })
-  }
-  
-  
 });
